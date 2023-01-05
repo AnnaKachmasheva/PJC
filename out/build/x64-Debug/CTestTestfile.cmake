923737cbb2047ec37c28d38ae0974bbcba16db32
Anna Kachmasheva <kachmann@fel.cvut.cz>
2023-01-05 14:45:29 +0100

    added v 0.0.0.4

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/mujprojekt-master
+# Build directory: C:/mujprojekt-master/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests "C:/mujprojekt-master/out/build/x64-Debug/tests.exe")
+set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "C:/mujprojekt-master/CMakeLists.txt;49;add_test;C:/mujprojekt-master/CMakeLists.txt;0;")
