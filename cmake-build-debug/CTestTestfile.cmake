544ec66fef013e16444896fbf2d65d1940c342fe
Anna Kachmasheva <kachmann@fel.cvut.cz>
2023-01-06 13:12:21 +0100

    added gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/mujprojekt-master
-# Build directory: C:/mujprojekt-master/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "C:/mujprojekt-master/cmake-build-debug/tests.exe")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "C:/mujprojekt-master/CMakeLists.txt;49;add_test;C:/mujprojekt-master/CMakeLists.txt;0;")
